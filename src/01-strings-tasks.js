e881d196027f3c8f326954fbb464108320799b3a
Anton Belski <anton.belski@gmail.com>
2023-05-24 14:53:15 +0300

    feat: add solution task

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -101,8 +101,8 @@ function getFirstChar(value) {
  *   'cat'              => 'cat'
  *   '\tHello, World! ' => 'Hello, World!'
  */
-function removeLeadingAndTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeLeadingAndTrailingWhitespaces( value) {
+   return value.trim()
 }
 
 /**
