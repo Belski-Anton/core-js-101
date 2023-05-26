b837784c7f64e47e7571862b11cba9d53543efd6
Anton Belski <anton.belski@gmail.com>
2023-05-26 12:36:55 +0300

    feat:add solution task

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -291,8 +291,19 @@ function isString(value) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const deck = [
+    'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+    'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠',
+  ];
+
+  const cardIndex = deck.indexOf(value);
+  if (cardIndex === -1) {
+    throw new Error(`Invalid card value: ${value}`);
+  }
+  return cardIndex;
 }
 
 
