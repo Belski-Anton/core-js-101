9d85a1203f681f072e94a21e7ef8f767a1da2c50
Anton Belski <anton.belski@gmail.com>
2023-05-29 12:33:39 +0300

    feat:add solution task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -115,8 +115,18 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dotProduct = x1 * x2 + y1 * y2;
+  const magnitude1 = Math.sqrt(x1 * x1 + y1 * y1);
+  const magnitude2 = Math.sqrt(x2 * x2 + y2 * y2);
+
+  let angle = Math.acos(dotProduct / (magnitude1 * magnitude2));
+
+  if (Number.isNaN(angle)) {
+    angle = 0;
+  }
+
+  return angle;
 }
 
 /**
