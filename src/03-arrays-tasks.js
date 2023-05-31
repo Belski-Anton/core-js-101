17eeb11d0ff6426d0b0e2bdd28ca45ff6b4ef598
Anton Belski <anton.belski@gmail.com>
2023-05-31 23:03:51 +0300

    feat:add solution task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -20,9 +20,11 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  const index = arr.indexOf(value);
+  return index !== -1 ? index : -1;
 }
+
 
 /**
  * Generates an array of odd numbers of the specified length
@@ -35,8 +37,9 @@ function findElement(/* arr, value */) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return new Array(len).fill(0)
+    .map((_, i) => 2 * i + 1);
 }
 
 
