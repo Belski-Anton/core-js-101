a1a185adc4d050ddd5a3cd7f4aaca0abfdfbc48a
Anton Belski <anton.belski@gmail.com>
2023-06-02 13:20:33 +0300

    feat:add solution task

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -105,8 +105,19 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hour = date.getUTCHours() % 12;
+  const minute = date.getUTCMinutes();
+
+  const hourAngle = 0.5 * (60 * hour + minute);
+  const minuteAngle = 6 * minute;
+
+  let angle = Math.abs(hourAngle - minuteAngle);
+  if (angle > 180) {
+    angle = 360 - angle;
+  }
+
+  return angle * (Math.PI / 180);
 }
 
 
