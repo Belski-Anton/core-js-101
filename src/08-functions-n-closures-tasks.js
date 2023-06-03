19df2993b069d6b2aaec114e1d5dca1f8067e963
Anton Belski <anton.belski@gmail.com>
2023-06-03 16:17:24 +0300

    feat:add solutions task

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,10 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return function composedFunction(x) {
+    return f(g(x));
+  };
 }
 
 
@@ -169,8 +171,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return function getNextId() {
+    const currentId = id;
+    id += 1;
+    return currentId;
+  };
 }
 
 
