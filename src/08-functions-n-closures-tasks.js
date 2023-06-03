bd849fe3643bd53fca292a2c3ccabae97013ea72
Anton Belski <anton.belski@gmail.com>
2023-06-03 16:31:22 +0300

    feat:add solutions task

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -66,8 +66,18 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...coefficients) {
+  if (coefficients.length === 0) {
+    return null;
+  }
+
+  return function polynomFunction(x) {
+    let result = 0;
+    for (let i = 0; i < coefficients.length; i += 1) {
+      result += coefficients[i] * (x ** (coefficients.length - 1 - i));
+    }
+    return result;
+  };
 }
 
 
@@ -85,8 +95,15 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache = null;
+
+  return function memoizedFunction() {
+    if (cache === null) {
+      cache = func();
+    }
+    return cache;
+  };
 }
 
 
